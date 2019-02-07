72c7ec6c4a65e62b94e1919b680b33ace76a636e
NicholasTHall <38834779+NicholasTHall@users.noreply.github.com>
2019-02-06 22:08:47 -0500

    Visual Studio Desktop

---
+++ Phase4/Desktop/CapstoneDesktopApp/CapstoneDesktopApp/AddEditForm.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace CapstoneDesktopApp
+{
+    partial class AddEditForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // AddEditForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1345, 781);
+            this.Name = "AddEditForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
