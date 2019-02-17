944cf359e67d66de367976c2f822bd1e10db2800
sjt5370 <47046057+sjt5370@users.noreply.github.com>
2019-02-17 02:06:53 -0500

    Add files via upload

--- Phase4/Stocking/java/edu/psu/sjt5370/stockingproto/DatabaseManager.java
+++ Phase4/Stocking/java/edu/psu/sjt5370/stockingproto/DatabaseManager.java
@@ -1,29 +1,33 @@
 package edu.psu.sjt5370.stockingproto;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.AsyncTask;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 public class DatabaseManager extends SQLiteOpenHelper {
     interface OnDatabaseReadyListener {
         void onDatabaseReady(SQLiteDatabase db);
     }
 
-    private static DatabaseManager manager;
+    private static DatabaseManager instance;
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "";
+    private static ArrayList<String> addProductQueue;
 
     private DatabaseManager(Context context) {
         super(context.getApplicationContext(), DATABASE_NAME, null, DATABASE_VERSION);
+        addProductQueue = new ArrayList<>();
     }
 
     public static synchronized DatabaseManager getInstance(Context context) {
-        if (manager == null) manager = new DatabaseManager(context);
-        return manager;
+        if (instance == null) instance = new DatabaseManager(context);
+        return instance;
     }
 
     private static final String CLEAR_TABLES =
@@ -70,7 +74,7 @@ public class DatabaseManager extends SQLiteOpenHelper {
         @Override
         protected SQLiteDatabase doInBackground(OnDatabaseReadyListener... params) {
             listener = params[0];
-            return DatabaseManager.manager.getWritableDatabase();
+            return DatabaseManager.instance.getWritableDatabase();
         }
         @Override
         protected void onPostExecute(SQLiteDatabase db) {
@@ -78,4 +82,54 @@ public class DatabaseManager extends SQLiteOpenHelper {
         }
     }
 
+    public void addProduct(Product product, SQLiteDatabase db) {
+        StringBuilder command = new StringBuilder();
+        command.append("insert into product values (");
+        command.append(product.getID());
+        command.append(", \'");
+        command.append(product.getProductName());
+        command.append("\', \'");
+        command.append(product.getDescription());
+        command.append("\', \'");
+        command.append(product.getManufacturer());
+        command.append("\', ");
+        command.append(product.getPrice());
+        command.append(");");
+        db.rawQuery(command.toString(), null);
+    }
+
+    public Product getProduct(int id, SQLiteDatabase db) {
+        StringBuilder command = new StringBuilder();
+        command.append("select prod_name, prod_desc, manu, price, inv_bulk, inv_shelf ");
+        command.append("from product, inventory ");
+        command.append("where product.prod_id = inventory.prod_id and product.prod_id = ");
+        command.append(id);
+        command.append(";");
+        Cursor c = db.rawQuery(command.toString(), null);
+        Product product = new Product();
+        product.setID(c.getInt(c.getColumnIndex("prod_id")));
+        product.setProductName(c.getString(c.getColumnIndex("prod_name")));
+        product.setDescription(c.getString(c.getColumnIndex("prod_desc")));
+        product.setManufacturer(c.getString(c.getColumnIndex("manu")));
+        product.setPrice(c.getDouble(c.getColumnIndex("price")));
+        product.setBulkStock(c.getInt(c.getColumnIndex("inv_bulk")));
+        product.setShelfStock(c.getInt(c.getColumnIndex("inv_shelf")));
+        c.close();
+        return product;
+    }
+
+    public boolean authenticate(int id, String password, SQLiteDatabase db) {
+        StringBuilder command = new StringBuilder();
+        command.append("select acc_type, password, job ");
+        command.append("from master_account, employee_account ");
+        command.append("where master_account.acc_id = employee_account.acc_id and account.id = ");
+        command.append(id);
+        command.append(";");
+        Cursor c = db.rawQuery(command.toString(), null);
+        boolean acc_type = (c.getInt(c.getColumnIndex("acc_type")) == 1);   //FIXME: true for employee or customer?
+        String correctPass = c.getString(c.getColumnIndex("password"));     //FIXME: needs encryption
+        String job = c.getString(c.getColumnIndex("job"));
+        c.close();
+        return acc_type && job.toLowerCase().equals("stocker") && password.equals(correctPass);
+    }
 }
--- Phase4/Stocking/java/edu/psu/sjt5370/stockingproto/Product.java
+++ Phase4/Stocking/java/edu/psu/sjt5370/stockingproto/Product.java
@@ -6,31 +6,61 @@ import android.os.Parcelable;
 import java.util.Random;
 
 public class Product implements Parcelable {
-    int bulkStock;
-    int shelfStock;
+    Integer id;
     String name;
-    static int count = 0;
-
+    String description;
+    String manufacturer;
+    Double price;
+    Integer bulkStock;
+    Integer shelfStock;
+    //static Integer count = 0;
+    /*
     public Product() {
         Random rand = new Random();
         bulkStock = rand.nextInt(1000) + 1;
         shelfStock = 0;
         name = "Example Product " + ++count;
     }
+    */
+    public Product() {}
     private Product(Parcel parcel) {
+        id = parcel.readInt();
+        name = parcel.readString();
+        description = parcel.readString();
+        manufacturer = parcel.readString();
+        price = parcel.readDouble();
         bulkStock = parcel.readInt();
         shelfStock = parcel.readInt();
-        name = parcel.readString();
     }
+
+    public Integer getID() { return id; }
+    public String getProductName() { return name; }
+    public String getDescription() { return description; }
+    public String getManufacturer() { return manufacturer; }
+    public Double getPrice() { return price; }
+    public Integer getBulkStock() { return bulkStock; }
+    public Integer getShelfStock() { return shelfStock; }
+
+    public void setID(int id) { this.id = id; }
+    public void setProductName(String name) { this.name = name; }
+    public void setDescription(String description) { this.description = description; }
+    public void setManufacturer(String manufacturer) { this.manufacturer = manufacturer; }
+    public void setPrice(double price) { this.price = price; }
+    public void setBulkStock(int bulkStock) { this.bulkStock = bulkStock; }
+    public void setShelfStock(int shelfStock) { this.shelfStock = shelfStock; }
 
     public boolean equals(Product that) { return this.name.equals(that.name); }
 
     public int describeContents() { return 0; }
 
     public void writeToParcel(Parcel out, int flags) {
+        out.writeInt(id);
+        out.writeString(name);
+        out.writeString(description);
+        out.writeString(manufacturer);
+        out.writeDouble(price);
         out.writeInt(bulkStock);
         out.writeInt(shelfStock);
-        out.writeString(name);
     }
 
     public static final Parcelable.Creator<Product> CREATOR = new Parcelable.Creator<Product>() {
