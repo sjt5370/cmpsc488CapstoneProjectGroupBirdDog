83a7ba2292bc4f74fae44771cb2f70ba1921a644
sjt5370 <47046057+sjt5370@users.noreply.github.com>
2019-02-18 04:55:05 -0500

    Add files via upload

--- Phase4/Stocking/java/edu/psu/sjt5370/stockingproto/DatabaseManager.java
+++ Phase4/Stocking/java/edu/psu/sjt5370/stockingproto/DatabaseManager.java
@@ -5,6 +5,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.AsyncTask;
+import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -17,12 +18,10 @@ public class DatabaseManager extends SQLiteOpenHelper {
 
     private static DatabaseManager instance;
     public static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "";
-    private static ArrayList<String> addProductQueue;
+    public static final String DATABASE_NAME = "stocking_database.db";
 
     private DatabaseManager(Context context) {
         super(context.getApplicationContext(), DATABASE_NAME, null, DATABASE_VERSION);
-        addProductQueue = new ArrayList<>();
     }
 
     public static synchronized DatabaseManager getInstance(Context context) {
@@ -38,19 +37,47 @@ public class DatabaseManager extends SQLiteOpenHelper {
                     "drop table if exists inventory;\n" +
                     "drop table if exists product;\n";
 
+    private static final String PROD_TABLE =
+            "create table product(prod_id int primary key, prod_name varchar(100), prod_desc varchar(255), manu varchar(50), price float, unique( prod_name, prod_desc, manu));";
+    private static final String INV_TABLE =
+            "create table inventory(prod_id int primary key, inv_bulk int not null, inv_shelf int not null, foreign key (prod_id) references product, check (not inv_bulk < 0), check (not inv_shelf < 0));";
+    private static final String MASTER_ACC_TABLE =
+            "create table master_account (acc_id int primary key, acc_type bit not null, password varchar(30) not null);";
+    private static final String EMPL_ACC_TABLE =
+            "create table employee_account (acc_id int primary key, job varchar(30), productivity int not null, foreign key(acc_id) references master_account);";
+    private static final String CUST_ACC_TABLE =
+            "create table customer_account (acc_id int primary key, acc_address varchar(255) not null, email varchar(50) not null, foreign key(acc_id) references master_account);";
+    private static final String ORDER_HIST_TABLE =
+            "create table order_history (acc_id int primary key, order_num int not null, prod_id int not null, quantity int not null, foreign key(acc_id) references customer_account, unique (order_num, prod_id), foreign key(prod_id) references product, check(not quantity < 0));";
+
     @Override
     public void onCreate(SQLiteDatabase db) {
+        /*
         String command;
         try {
+            String location = getClass().getClassLoader().getResource(".").getPath() + "app/src/main/text/create_tables.txt";
             StringBuilder sb = new StringBuilder();
             String line;
-            BufferedReader br = new BufferedReader(new FileReader("../../../../../text/create_tables.txt"));
+            BufferedReader br = new BufferedReader(new FileReader(location));
             while ((line = br.readLine()) != null) sb.append(line);
             command = sb.toString();
+            System.out.println(command);
+            db.execSQL(command);
+            createTempAccount();
         } catch (Exception fnfex) {
-            command = "";
+            Log.e("DatabaseManager", "Table failed to initialize.");
+            System.exit(1);
         }
-        db.execSQL(command);
+        */
+        db.execSQL(PROD_TABLE);
+        db.execSQL(INV_TABLE);
+        db.execSQL(MASTER_ACC_TABLE);
+        db.execSQL(EMPL_ACC_TABLE);
+        db.execSQL(CUST_ACC_TABLE);
+        db.execSQL(ORDER_HIST_TABLE);
+
+        db.execSQL("insert into master_account values (12345, 1, 'password');");
+        db.execSQL("insert into employee_account values (12345, 'stocker', 0);");
     }
 
     @Override
@@ -68,6 +95,8 @@ public class DatabaseManager extends SQLiteOpenHelper {
        new OpenDatabaseAsyncTask().execute(listener);
     }
 
+
+
     public class OpenDatabaseAsyncTask extends AsyncTask<OnDatabaseReadyListener, Void, SQLiteDatabase> {
         OnDatabaseReadyListener listener;
 
@@ -81,6 +110,8 @@ public class DatabaseManager extends SQLiteOpenHelper {
             listener.onDatabaseReady(db);
         }
     }
+
+
 
     public void addProduct(Product product, SQLiteDatabase db) {
         StringBuilder command = new StringBuilder();
@@ -96,6 +127,16 @@ public class DatabaseManager extends SQLiteOpenHelper {
         command.append(product.getPrice());
         command.append(");");
         db.rawQuery(command.toString(), null);
+
+        command = new StringBuilder();
+        command.append("insert into inventory values (");
+        command.append(product.getID());
+        command.append(", ");
+        command.append(product.getBulkStock());
+        command.append(", ");
+        command.append(product.getShelfStock());
+        command.append(");");
+        db.rawQuery(command.toString(), null);
     }
 
     public Product getProduct(int id, SQLiteDatabase db) {
@@ -106,6 +147,10 @@ public class DatabaseManager extends SQLiteOpenHelper {
         command.append(id);
         command.append(";");
         Cursor c = db.rawQuery(command.toString(), null);
+        if (!c.moveToFirst()) {
+            c.close();
+            return null;
+        }
         Product product = new Product();
         product.setID(c.getInt(c.getColumnIndex("prod_id")));
         product.setProductName(c.getString(c.getColumnIndex("prod_name")));
@@ -118,14 +163,52 @@ public class DatabaseManager extends SQLiteOpenHelper {
         return product;
     }
 
+    public ArrayList<Product> getProductList(SQLiteDatabase db) {
+        Cursor c = db.rawQuery("select prod_name, prod_desc, manu, price, inv_bulk, inv_shelf from product, inventory where product.prod_id = inventory.prod_id;", null);
+        ArrayList<Product> productList = new ArrayList<>();
+        if (!c.moveToFirst()) {
+            c.close();
+            return null;
+        }
+        for (; !c.isAfterLast(); c.moveToNext()) {
+            Product product = new Product();
+            product.setID(c.getInt(c.getColumnIndex("prod_id")));
+            product.setProductName(c.getString(c.getColumnIndex("prod_name")));
+            product.setDescription(c.getString(c.getColumnIndex("prod_desc")));
+            product.setManufacturer(c.getString(c.getColumnIndex("manu")));
+            product.setPrice(c.getDouble(c.getColumnIndex("price")));
+            product.setBulkStock(c.getInt(c.getColumnIndex("inv_bulk")));
+            product.setShelfStock(c.getInt(c.getColumnIndex("inv_shelf")));
+            productList.add(product);
+        }
+        c.close();
+        return productList;
+    }
+
+    public void stockProduct(int id, int quantity, SQLiteDatabase db) {
+        StringBuilder command = new StringBuilder();
+        command.append("update inventory set inv_bulk = inv_bulk - ");
+        command.append(quantity);
+        command.append(", inv_shelf = inv_shelf + ");
+        command.append(quantity);
+        command.append(" where prod_id = ");
+        command.append(id);
+        command.append(";");
+        db.rawQuery(command.toString(), null);
+    }
+
     public boolean authenticate(int id, String password, SQLiteDatabase db) {
         StringBuilder command = new StringBuilder();
         command.append("select acc_type, password, job ");
         command.append("from master_account, employee_account ");
-        command.append("where master_account.acc_id = employee_account.acc_id and account.id = ");
+        command.append("where master_account.acc_id = employee_account.acc_id and master_account.acc_id = ");
         command.append(id);
         command.append(";");
         Cursor c = db.rawQuery(command.toString(), null);
+        if (!c.moveToFirst()) {
+            c.close();
+            return false;
+        }
         boolean acc_type = (c.getInt(c.getColumnIndex("acc_type")) == 1);   //FIXME: true for employee or customer?
         String correctPass = c.getString(c.getColumnIndex("password"));     //FIXME: needs encryption
         String job = c.getString(c.getColumnIndex("job"));
--- Phase4/Stocking/java/edu/psu/sjt5370/stockingproto/LoginActivity.java
+++ Phase4/Stocking/java/edu/psu/sjt5370/stockingproto/LoginActivity.java
@@ -1,17 +1,25 @@
 package edu.psu.sjt5370.stockingproto;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {  //FIXME: All back buttons should be custom to avoid restarting activities
 
+    private SQLiteDatabase database;
+    private boolean authPending;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        authPending = false;
         setContentView(R.layout.activity_login);
         ((Button) findViewById(R.id.forgotPasswordButton)).setOnClickListener(
                 new View.OnClickListener() {
@@ -25,12 +33,42 @@ public class LoginActivity extends AppCompatActivity {  //FIXME: All back button
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        Intent intent = new Intent(LoginActivity.this, StockingActivity.class);
-                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                        startActivity(intent);
-                        finish();
+                        authenticate();
+                        //FIXME: Add loading gif
                     }
                 }
         );
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        DatabaseManager.getInstance(this).getWritableDatabase(new DatabaseManager.OnDatabaseReadyListener() {
+            @Override
+            public void onDatabaseReady(SQLiteDatabase db) {
+                database = db;
+                if (authPending) authenticate();
+            }
+        });
+    }
+
+    private void authenticate() {
+        if (database == null) {
+            authPending = true;
+            return;
+        }
+        authPending = false;
+        Editable id = ((EditText) findViewById(R.id.emailField)).getText();
+        Editable password = ((EditText) findViewById(R.id.passwordField)).getText();
+        if (!TextUtils.isEmpty(id) && !TextUtils.isEmpty(password) && DatabaseManager.getInstance(this).authenticate(Integer.parseInt(id.toString()), password.toString(), database)) {
+            Intent intent = new Intent(LoginActivity.this, StockingActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            if (database != null) database.close();
+            finish();
+        } else {
+            //FIXME: Highlight textboxes as invalid
+            Toast.makeText(LoginActivity.this, getResources().getString(R.string.login_failed), Toast.LENGTH_LONG).show();
+        }
     }
 }
