a2b45a1b0584bcafcaca65c915cd109430911c22
NicholasTHall <38834779+NicholasTHall@users.noreply.github.com>
2019-02-26 23:22:16 -0500

    java code for populating the database

---
+++ Tester.java
@@ -0,0 +1,44 @@
+
+package tester;
+import java.util.Scanner;
+/**
+ *
+ * @author Nicholas
+ */
+public class Tester {
+
+    public static void main(String[] args) {
+        
+        int n = 1;
+        int zero = 0;
+        for(int i = 0; i < 1000; i++)
+        {
+            n = n + 1;
+            System.out.print("Insert into product values (" + n + ", 'product" + n + 
+                    "', 'this is product1', 'manufacture" + n + "');");
+            System.out.println("");
+            
+            System.out.print("Insert into inventory values (" + n + ", " + n + ", " +
+                    n + ", " + zero + ");");                    
+            System.out.println("");
+            
+            zero = (zero + n) % 2;
+            System.out.print("Insert into master_account values (" + n + ", " + zero + 
+                    ", 'User" + n + "', 'password" + n + "');");                    
+            System.out.println("");
+            
+            if(zero == 0){
+                System.out.print("Insert into employee_account values (" + n + 
+                        ", 'Tester" + n + "', 'employee', 'temp', " + n + ");");                    
+                System.out.println("");
+            }
+            
+            if(zero == 1){
+                System.out.print("Insert into customer_account values (" + n + 
+                        ", '" + n + " tester ave', 'abc1234atpsu');");                    
+                System.out.println("");
+            }
+        }
+    }
+    
+}
