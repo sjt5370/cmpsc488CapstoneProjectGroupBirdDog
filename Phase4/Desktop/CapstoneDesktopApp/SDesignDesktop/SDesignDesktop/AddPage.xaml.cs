ce46227334fa4da7b1fc72838969da43a657c7c0
Campyx96 <45395077+Campyx96@users.noreply.github.com>
2019-03-13 21:58:39 -0400

    updated desktop app

---
+++ Phase4/Desktop/CapstoneDesktopApp/SDesignDesktop/SDesignDesktop/AddPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SDesignDesktop
+{
+    /// <summary>
+    /// Interaction logic for AddPage.xaml
+    /// </summary>
+    public partial class AddPage : Page
+    {
+        public AddPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Confirm_Click(object sender, RoutedEventArgs e)
+        {
+            Add.Content = new ViewPage();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Add.Content = new ViewPage();
+        }
+    }
+}
---
+++ Phase4/Desktop/CapstoneDesktopApp/SDesignDesktop/SDesignDesktop/EditPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SDesignDesktop
+{
+    /// <summary>
+    /// Interaction logic for EditPage.xaml
+    /// </summary>
+    public partial class EditPage : Page
+    {
+        public EditPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Edit.Content = new ViewPage();
+        }
+
+        private void Done_Click(object sender, RoutedEventArgs e)
+        {
+            Edit.Content = new ViewPage();
+        }
+    }
+}
