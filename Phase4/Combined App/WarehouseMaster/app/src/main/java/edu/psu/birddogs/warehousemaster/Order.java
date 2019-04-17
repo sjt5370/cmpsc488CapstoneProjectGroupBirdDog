ea81cb5e81c093b576c001d654ac20ce3735c068
jcf5277 <jcf5277@psu.edu>
2019-04-16 23:06:53 -0400

    Added Priority sorting for Order Page, fixed issue with clicking to display product description.

--- Phase4/Combined App/WarehouseMaster/app/src/main/java/edu/psu/birddogs/warehousemaster/Order.java
+++ Phase4/Combined App/WarehouseMaster/app/src/main/java/edu/psu/birddogs/warehousemaster/Order.java
@@ -3,6 +3,8 @@ package edu.psu.birddogs.warehousemaster;
 import android.app.ActionBar.LayoutParams;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,11 +16,13 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Set;
 
 public class Order extends AppCompatActivity {
     private HashMap<Integer, PickProduct> pallet;
+    private ArrayList<ArrayList<Integer>> priorities = new ArrayList<>();
     private boolean firstTime = true;
     private boolean finishing = false;
     public static int returnedId = -1;
@@ -42,31 +46,75 @@ public class Order extends AppCompatActivity {
 
                 if(firstTime) {
                     prods = pallet.keySet();
+
+                    //take ids of products and sort them by priority
+                    for(int i = 0; i < 10; i++) {
+                        priorities.add(new ArrayList<Integer>());
+                    }
                     for(Integer prod : prods){
-                        CheckBox c = new CheckBox(Order.this);
-                        TextView v = new TextView(Order.this);
+                        priorities.get(pallet.get(prod).getPriority()).add(prod);
+                    }
 
-                        String txt = pallet.get(prod).getDescription() + '\n' + getResources().getString(R.string.manufacturer_label) + ": " + pallet.get(prod).getManu();
-                        v.setText(txt);
-                        v.setId(100000 + prod);
-                        v.setPadding(100, 10, 5, 10);
-                        v.setVisibility(View.GONE);
+                    //create priority textViews
+                    for(int i = 0; i < 10; i++){
+                        priorities.add(new ArrayList<Integer>());
 
-                        c.setText(pallet.get(prod).getID() + ": " + pallet.get(prod).getName() + "     x" + pallet.get(prod).getQuantity());
-                        c.setId(prod);
-                        c.setGravity(Gravity.CENTER);
-                        c.setOnClickListener(new View.OnClickListener() {
+                        TextView p = new TextView(Order.this);
+
+                        String txt = "" + (i+1);
+                        switch (i){
+                            case 0: txt += "st"; break;
+                            case 1: txt += "nd"; break;
+                            case 2: txt += "rd"; break;
+                            case 9: txt = "last"; break;
+                            default: txt += "th"; break;
+                        }
+                        txt += " Priority";
+                        p.setText(txt);
+                        p.setTextColor(Color.BLACK);
+                        p.setTextSize(25);
+                        p.setTypeface(p.getTypeface(), Typeface.BOLD_ITALIC);
+                        p.setId(i);
+                        p.setGravity(Gravity.CENTER);
+                        p.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
-                                check(v, false);
+                                reveal(v);
                             }
                         });
-                        c.setClickable(false);
+                        lm.addView(p);
+                        for(Integer a : priorities.get(i)){
+                            CheckBox c = new CheckBox(Order.this);
+                            TextView v = new TextView(Order.this);
+
+                            String txt2 = pallet.get(a).getDescription() + '\n' + '\n' + getResources().getString(R.string.manufacturer_label) + ": " + pallet.get(a).getManu();
+                            v.setText(txt2);
+                            v.setId(100000 + a);
+                            v.setPadding(100, 10, 5, 10);
+                            v.setVisibility(View.GONE);
+
+                            c.setText(pallet.get(a).getID() + ": " + pallet.get(a).getName() + "     x" + pallet.get(a).getQuantity());
+                            c.setId(a);
+                            c.setGravity(Gravity.CENTER);
+                            c.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    check(v, false);
+                                }
+                            });
+                            //c.setClickable(false);
 
 
-                        //c.setLayoutParams(params);
-                        lm.addView(c);
-                        lm.addView(v);
+                            //c.setLayoutParams(params);
+                            lm.addView(c);
+                            lm.addView(v);
+                        }
+                    }
+                    for(int j = 0; j < 10; j++){
+                        TextView pr = (TextView) findViewById(j);
+                        if(priorities.get(j).size() == 0){
+                            pr.setVisibility(View.GONE);
+                        }
                     }
                     firstTime = false;
                 }
@@ -99,27 +147,54 @@ public class Order extends AppCompatActivity {
         int j = v.getId();
         PickProduct product = pallet.get(j);
         ((CheckBox) v).setChecked(b);
-        ((CheckBox) v).setClickable(b);
+
 
         j = j + 100000;
+
+        TextView t = (TextView) findViewById(j);
+
+        //set description visibility
+        if(t.getVisibility() == View.VISIBLE){
+            t.setVisibility(View.GONE);
+        }else {
+            t.setVisibility(View.VISIBLE);
+        }
+
         if (b) {
             if (!product.getScanned())
                 DatabaseManager.removeInventory(product.getID(), product.getQuantity());
             product.setScanned(true);
             returnedId = -1;
-            ((TextView) findViewById(j)).setVisibility(View.VISIBLE);
             for (Integer prod : prods)
                 if (!((CheckBox) findViewById(prod)).isChecked()) b = false;
         } else {
             if (product.getScanned())
                 DatabaseManager.returnInventory(product.getID(), product.getQuantity());
             product.setScanned(false);
-            ((TextView) findViewById(j)).setVisibility(View.GONE);
         }
 
         ((Button) findViewById(R.id.finishPalette)).setEnabled(b);
         ((Button) findViewById(R.id.holdButton)).setEnabled(!b);
         ((Button) findViewById(R.id.scanButton)).setEnabled(!b);
+    }
+    private void reveal(View v){
+        int id = v.getId();
+        int shown = findViewById(priorities.get(id).get(0)).getVisibility();
+        System.out.println(shown);
+        System.out.println(shown == 8);
+        if(shown == 8){
+                for(Integer a : priorities.get(id)){
+                    CheckBox c = (CheckBox) findViewById(a);
+                    c.setVisibility(View.VISIBLE);
+                }
+        }else{
+                for(Integer a : priorities.get(id)){
+                    CheckBox c = (CheckBox) findViewById(a);
+                    TextView t = (TextView) findViewById(100000 + a);
+                    c.setVisibility(View.GONE);
+                    t.setVisibility(View.GONE);
+                }
+        }
     }
 
     @Override
